02e5c2df6163ca3fdc0cbfb8ff243ddee8f581b0
George <gs.ivanov@abv.bg>
2022-10-11 08:00:05 +0300

    Add Dogs Controller

---
+++ Controllers/DogsController.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppMVC.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class DogsController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
